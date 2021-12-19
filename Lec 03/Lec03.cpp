4ecef1e1ac5f86759f65f2e3e03e2973d9733e3b
Sarthak Agarwal <93201986+SarthakAgarwal21@users.noreply.github.com>
2021-12-19 19:09:04 +0530

    Update Lec03.cpp
    Just some basic changes

--- Lec 03/Lec03.cpp
+++ Lec 03/Lec03.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 int main()
 {
-    //This line displays a message "Hello World" onto the screen 
-    cout<<"Hello World";
-    cout<<"This is a C++ class."; //This line displays another message onto the screen
+    cout<<"Hello World";                                //This line displays a message "Hello World" onto the screen 
+    cout<<"This is a C++ class.";                       //This line displays another message onto the screen
 
-    /* This is is a block This is another block comment inside comment */
+    /* This is is a block
+       This is another block comment inside comment */              //Can span multible lines using '/*'
 
-    int a = 5;
+    
+    int a = 5;                                           //int is used to show that 'a' is an integer value
     int b = 6;
 
-    cout<<a + b;
+    cout<<a + b;                                        
 
-    int x_yz$ = 5;
+    int x_yz$ = 5;                                       //we can use alphabets, _ and underscores as the name of the variables
 
     char c = 'A';
 
@@ -30,22 +31,24 @@ int main()
 
     int $xyz = 5;
 
-    char var = 91;     // code using ASCII 91 - [
+    char var = 91;                    // code using ASCII 91 - [
 
     cout<<endl<<var;
 
-    bool new_var = true;    //bool var type, will see the purpose of true and false in later lectures
+    bool new_var = true;               //bool var type, will see the purpose of true and false in later lectures
 
     cout<<endl<<new_var;
 
-    extern int s;           //delay reservation of memory(out of syllabus). declaration and definition not happening together. declaration is name a whereas definition is reservation of memory.
+    extern int s;                       //delay reservation of memory(out of syllabus). declaration and definition not happening together. declaration is name a whereas definition is reservation of memory.
     
     
         //end of lecture 3
+    
+    
         
 cout<<'\n'<<sizeof(76542L);
 
-        cout<<'\n'<<"Hello World\n";  //in new line, single qoute
+        cout<<'\n'<<"Hello World\n";           //in new line, single qoute
     cout<<"This is a C++ class.";
 
         cout<<'\n'<<"'Good' Morning";          //'Good' Morning
